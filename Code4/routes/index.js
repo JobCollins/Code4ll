3b4220d57bdc11cefd6dfddd949847624e80821c
JobCollins <dulo.collins@gmail.com>
2021-02-19 14:17:23 +0300

    Feature: Sendng Transactional Emails
    setup mail service for application, and can send emails

--- Code4/routes/index.js
+++ Code4/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var nodemailer = require('nodemailer');
+var config = require('../config');
+var transporter = nodemailer.createTransport(config.mailer);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Code4 - a platform for sharing code.' });
@@ -30,7 +34,20 @@ router.route('/contact')
         errorMessages: errors
       });
     } else {
-      res.render('thank', { title: 'Code4 - Thanks for getting in touch!' });
+      var mailOptions = {
+        from: 'Code4 <no-reply@code4.com>',
+        to: 'dulojob@gmail.com',
+        subject: 'New Message',
+        text: req.body.message
+      };
+
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          return console.log(error);
+        }
+        res.render('thank', { title: 'Code4 - Thanks for getting in touch!' });
+      });
+
     }   
   });
 
